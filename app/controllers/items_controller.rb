0d40cd0afaad53681eddc14b60f1b87a98957b30
tomoki0107 <tttttnozawa0340@gmail.com>
2020-11-14 01:31:06 +0900

    エラーメッセージの表示ともどるボタンの遷移先の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
       redirect_to item_path
     else
       render :edit
