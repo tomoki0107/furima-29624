6f1e0fd39edb1f402c64281dc38815ba8d360b2e
tomoki0107 <tttttnozawa0340@gmail.com>
2020-11-15 15:34:40 +0900

    条件分岐の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,12 +38,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id != @item.user.id
-      redirect_to root_path
-    else
+    if current_user.id == @item.user.id
       @item.destroy
-      redirect_to root_path
     end
+      redirect_to root_path
   end
 
   private
