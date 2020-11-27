013e48721857f807e870e00c9ee4baebd028ab99
tomoki0107 <tttttnozawa0340@gmail.com>
2020-11-27 18:11:16 +0900

    配送先情報を保存するための処理とエラーメッセージの表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,4 +3,20 @@ class PurchasesController < ApplicationController
   def index
     @user_purchase = UserPurchase.new
   end
+
+  def create
+    @user_purchase = UserPurchase.new(purchase_params)
+      if @user_purchase.valid?
+        @user_purchase.save
+        redirect_to root_path
+      else
+        render action: :index
+      end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:user_purchase).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
